a81c4ab8edf110c44683d93b032a4fa9ba30db85
diewland <diewland@gmail.com>
2025-03-14 15:47:29 +0700

    get url from prompt

--- app/src/main/java/com/diewland/fullscrenbrowser/MainActivity.kt
+++ app/src/main/java/com/diewland/fullscrenbrowser/MainActivity.kt
@@ -1,16 +1,24 @@
 package com.diewland.fullscrenbrowser
 
 import android.annotation.SuppressLint
+import android.app.AlertDialog
+import android.content.Context
 import android.os.Bundle
+import android.text.InputType
 import android.view.View
 import android.webkit.WebChromeClient
 import android.webkit.WebSettings
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import android.widget.EditText
 import androidx.activity.ComponentActivity
 
 class MainActivity : ComponentActivity() {
 
+    companion object {
+        private const val PREFS_NAME = "FsBrowser"
+        private const val KEY_TEXT = "url"
+    }
     private lateinit var webView: WebView
 
     @SuppressLint("SetJavaScriptEnabled")
@@ -28,7 +36,12 @@ class MainActivity : ComponentActivity() {
         webView.webViewClient = WebViewClient()
         webView.webChromeClient = WebChromeClient()
 
-        webView.loadUrl("https://www.youtube.com")
+        val url = loadURL()
+        if (url.isEmpty()) {
+            promptNewURL()
+        } else {
+            webView.loadUrl(url)
+        }
     }
 
     override fun onResume() {
@@ -36,6 +49,7 @@ class MainActivity : ComponentActivity() {
         hideSystemUI()
     }
 
+    // display
     private fun hideSystemUI() {
         window.decorView.systemUiVisibility = (
                 View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
@@ -45,5 +59,34 @@ class MainActivity : ComponentActivity() {
                         or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
                         or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
                 )
+    }
+
+    // prompt
+    private fun promptNewURL() {
+        val input = EditText(this)
+        input.inputType = InputType.TYPE_CLASS_TEXT
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Please input URL")
+            .setMessage("Ex: https://youtube.com")
+            .setView(input)
+            .setPositiveButton("OK") { _, _ ->
+                val newURL = input.text.toString()
+                saveURL(newURL)
+                webView.loadUrl(newURL)
+            }
+            .setNegativeButton("Cancel", null)
+            .show()
+    }
+
+    // local storage
+    private fun saveURL(text: String) {
+        val sharedPreferences = getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+        editor.putString(KEY_TEXT, text)
+        editor.apply()
+    }
+    private fun loadURL(): String {
+        val sharedPreferences = getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE)
+        return sharedPreferences.getString(KEY_TEXT, "") ?: ""
     }
 }
